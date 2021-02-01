f19cb41a2d0c62b5a9e9dd73f9b79211e80800fe
liupf <1963221828@qq.com>
2021-02-01 17:54:36 +0800

    hooks学习
    Signed-off-by: liupf <“1963221828@qq.com”>

--- src/pages/sys/demo/index.js
+++ src/pages/sys/demo/index.js
@@ -4,15 +4,27 @@
  * @Author: liupf
  * @Date: 2021-02-01 15:57:34
  * @LastEditors: liupf
- * @LastEditTime: 2021-02-01 16:23:46
+ * @LastEditTime: 2021-02-01 17:35:46
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { Page } from '@components';
+
+function Example() {
+  // 声明一个新的叫做 “count” 的 state 变量
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
 
 function Welcome(props) {
   return (
     <Page>
-      <h1>Hello</h1>
+      <Example />
     </Page>
   );
 }
